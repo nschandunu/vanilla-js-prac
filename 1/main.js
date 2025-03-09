a5cc391ff5027c1e69591d57d6884379245f905c
Senuka Chandunu <senukaacc@gmail.com>
2025-03-09 22:41:01 +0530

    Let, const

---
+++ 1/main.js
@@ -0,0 +1,3 @@
+console.log("Hey");
+console.error("This is a error!");
+console.warn("This is a warning!");
---
+++ 2/main.js
@@ -0,0 +1,10 @@
+//  let, const
+
+let age = 30;
+console.log("Age is, ", age);
+
+let x = 10;
+x = 20;
+
+const y = 30;  // fixed value
+// y = 40; Error (cannot reassign a const variable)
