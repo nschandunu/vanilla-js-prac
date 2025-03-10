6812a8d3c22826800a1c3323872925de4f8d6906
Senuka Chandunu <senukaacc@gmail.com>
2025-03-10 23:23:18 +0530

    strings

---
+++ 4/main.js
@@ -0,0 +1,19 @@
+// string, number, boolean, undefined, null
+
+const name = 'Senuka';
+const age = 25;
+
+
+// Concatenation
+console.log('My name is ' + name + ' and I am ' + age);
+console.log(`My name is ${name} and I am ${age}`);
+
+const s = "Hello World";
+console.log(s.length);
+console.log(s.toUpperCase()); 
+console.log(s.substring(0,5));  // first 5 letters
+console.log(s.substring(0,5).toUpperCase());
+console.log(s.split(''))
+
+const z = 'tech, computers, it, code';
+console.log(z.split(', '));
